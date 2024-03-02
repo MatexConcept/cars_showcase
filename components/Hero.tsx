ff8e5ae13ff16827f82b630cf489960f2d854776
MatexConcept <matexconcept@gmail.com>
2024-03-02 11:44:56 +0100

    second commit

---
+++ components/Hero.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+const Hero = () => {
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+       
+Discover and rent a car quickly!
+        </h1>
+
+        <p className="hero__subtitle">
+          Simplify the process of renting a car with our easy and streamlined
+          booking system.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          // containerStyles="bg-custom-brown text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+
+          <div className="hero__image-overlay" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
