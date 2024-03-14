e33a28a040d39e8acc9debe11d960fa19c0cf1a8
MatexConcept <matexconcept@gmail.com>
2024-03-14 22:48:14 +0100

    commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">Discover and rent a car quickly!</h1>
+        <h1 className="hero__title">Discover and rent a car quickly</h1>
 
         <p className="hero__subtitle">
           Simplify the process of renting a car with our easy and streamlined
